588aa4cd4f60d897c0e0f8e9e9449545ffa6deb8
Muhammad Ridho Al Zamzami <workplus@192.168.1.13>
2026-03-24 03:41:05 +0700

    update docker

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+Route::get('/{any?}', function () {
+    return view('app');
+})->where('any', '^(?!api).*$');
 
 require __DIR__.'/auth.php';

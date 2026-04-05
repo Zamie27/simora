e0298e5ae33ce1c17b1986189dddf67b4a769568
Zamie27 <zamie693@gmail.com>
2026-04-05 08:21:17 +0700

    feat: update logo component to display icon on mobile and full logo on desktop

--- resources/js/components/AppLogoIcon.vue
+++ resources/js/components/AppLogoIcon.vue
@@ -13,10 +13,18 @@ defineProps<Props>();
 </script>
 
 <template>
-    <img
-        src="/images/simora_logo.png"
-        alt="Simora Logo"
-        :class="['object-contain', className]"
-        v-bind="$attrs"
-    />
+    <div :class="['flex items-center', className]" v-bind="$attrs">
+        <!-- Full Logo (md and up) -->
+        <img
+            src="/images/simora_logo.png"
+            alt="Simora Logo"
+            class="hidden h-full w-auto object-contain md:block"
+        />
+        <!-- Small Icon (below md) -->
+        <img
+            src="/images/simora_icon.png"
+            alt="Simora Icon"
+            class="block h-full w-auto object-contain md:hidden"
+        />
+    </div>
 </template>
